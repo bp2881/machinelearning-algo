4c51ad5aab106d224ea61930ad2078ce6eb5bc7f
Pranav <pranavbairy2@gmail.com>
2026-01-03 23:16:02 +0530

    Created a base

--- preprocessing.r
+++ preprocessing.r
@@ -1,6 +1,6 @@
 ### House Price dataset
 #hp <- read.csv("/home/pranav/Downloads/coding/projects/machinelearning-algo/house_price_old.csv", sep = ";", header = TRUE)
-hp <-read.csv("/home/pranav/Downloads/coding/projects/machinelearning-algo_grade/RealEstate.csv")
+hp <-read.csv("/home/pranav/Downloads/coding/projects/machinelearning-algo_pytorch/RealEstate.csv")
 nrow(hp) # 414
 ncol(hp) # 8
 
@@ -12,5 +12,5 @@ summary(hp)
 str(hp)
 
 cor(hp$X2.house.age, hp$Y.house.price.of.unit.area) # -0.21
-cor(hp$X4.number.of.convenience.stores, hp$Y.house.price.of.unit.area) # 0.57  [ THE LIKLIEST :/ according to intuition ]
-cor(hp$X3.distance.to.the.nearest.MRT.station, hp$Y.house.price.of.unit.area) # -0.67 
+cor(hp$X4.number.of.convenience.stores, hp$Y.house.price.of.unit.area) # 0.57  
+cor(hp$X3.distance.to.the.nearest.MRT.station, hp$Y.house.price.of.unit.area) # -0.67 [ THE LIKLIEST :/ ]
